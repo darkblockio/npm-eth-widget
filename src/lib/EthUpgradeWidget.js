3686f62d6c2a4cf3d5f823d7f52e74ab9805f513
Gary Strader <gstrader28@yahoo.com>
2022-08-08 16:28:54 -0500

    More add content updates

--- src/lib/EthUpgradeWidget.js
+++ src/lib/EthUpgradeWidget.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { useMachine } from "@xstate/react"
 import { utils, Upgrader, upgradeMachine } from "./shared"
+import signTypedData from "../utils/signTypedData"
 
 const platform = "Ethereum"
 const EthUpgradeWidget = ({
@@ -54,6 +55,7 @@ const EthUpgradeWidget = ({
 
           if (checkAddress) {
             setAddress(checkAddress)
+            state.context.wallet_address = checkAddress
             send({ type: "CONNECT_WALLET" })
           } else {
             send({ type: "CONNECT_FAILED" })
@@ -70,6 +72,13 @@ const EthUpgradeWidget = ({
       if (state.value === "verify_owner") {
         verifyOwnership()
       }
+
+      if (state.value === "signing") {
+        console.log("^^^^^^^^^^^^^^eth signing")
+        console.log("state to sign", state)
+        signFileUploadData()
+      }
+
       //
       // if (state.value === "signing") {
       //   console.log("------ signing")
@@ -145,72 +154,89 @@ const EthUpgradeWidget = ({
     }
   }
 
-  const signFileUploadData = async (address, data, w3) => {
-    return new Promise((resolve, reject) => {
-      try {
-        const msgParams = JSON.stringify({
-          domain: {
-            // Defining the chain aka Rinkeby testnet or Ethereum Main Net
-            chainId: 1, //ethereum
-            // Give a user friendly name to the specific contract you are signing for.
-            name: "Verifying Ownership",
-            // If name isn't enough add verifying contract to make sure you are establishing contracts with the proper entity
-            verifyingContract: address,
-            // Just let's you know the latest version. Definitely make sure the field name is correct.
-            version: "1",
-          },
+  const signFileUploadData = async () => {
+    let signatureData = `${state.context.platform}${state.context.nftData.nft.contract}:${state.context.nftData.nft.token}${state.context.fileHash}`
 
-          // Defining the message signing data content.
-          message: {
-            /*
-               - Anything you want. Just a JSON Blob that encodes the data you want to send
-               - No required fields
-               - This is DApp Specific
-               - Be as explicit as possible when building out the message schema.
-              */
-            contents: data,
-          },
-          // Refers to the keys of the *types* object below.
-          primaryType: "Mail",
-          types: {
-            // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
-            EIP712Domain: [
-              { name: "name", type: "string" },
-              { name: "version", type: "string" },
-              { name: "chainId", type: "uint256" },
-              { name: "verifyingContract", type: "address" },
-            ],
+    console.log("signatureData", signatureData)
 
-            // Refer to PrimaryType
-            Mail: [{ name: "contents", type: "string" }],
-            // Not an EIP712Domain definition
-          },
-        })
-
-        setTimeout(() => {
-          w3.currentProvider.sendAsync(
-            {
-              method: "eth_signTypedData_v4",
-              params: [address, msgParams],
-              from: address,
-            },
-            async function (err, result) {
-              if (err) reject(null)
-              if (result.error) {
-                reject(null)
-              }
-              resolve(result.result)
-            }
-          )
-        }, 1)
-      } catch (err) {
-        resolve(err)
-      }
+    let signature = await signTypedData(signatureData, w3).then((response) => {
+      return response
     })
+
+    console.log("signature", signature)
+
+    state.context.signature = signature
+    state.context.uploadPercent = 50
+
+    console.log("ETH WIDGET state", state)
+
+    send({ type: "SUCCESS" })
+
+    // return new Promise((resolve, reject) => {
+    //   try {
+    //     const msgParams = JSON.stringify({
+    //       domain: {
+    //         // Defining the chain aka Rinkeby testnet or Ethereum Main Net
+    //         chainId: 1, //ethereum
+    //         // Give a user friendly name to the specific contract you are signing for.
+    //         name: "Verifying Ownership",
+    //         // If name isn't enough add verifying contract to make sure you are establishing contracts with the proper entity
+    //         verifyingContract: address,
+    //         // Just let's you know the latest version. Definitely make sure the field name is correct.
+    //         version: "1",
+    //       },
+    //
+    //       // Defining the message signing data content.
+    //       message: {
+    //         /*
+    //            - Anything you want. Just a JSON Blob that encodes the data you want to send
+    //            - No required fields
+    //            - This is DApp Specific
+    //            - Be as explicit as possible when building out the message schema.
+    //           */
+    //         contents: data,
+    //       },
+    //       // Refers to the keys of the *types* object below.
+    //       primaryType: "Mail",
+    //       types: {
+    //         // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
+    //         EIP712Domain: [
+    //           { name: "name", type: "string" },
+    //           { name: "version", type: "string" },
+    //           { name: "chainId", type: "uint256" },
+    //           { name: "verifyingContract", type: "address" },
+    //         ],
+    //
+    //         // Refer to PrimaryType
+    //         Mail: [{ name: "contents", type: "string" }],
+    //         // Not an EIP712Domain definition
+    //       },
+    //     })
+    //
+    //     setTimeout(() => {
+    //       w3.currentProvider.sendAsync(
+    //         {
+    //           method: "eth_signTypedData_v4",
+    //           params: [address, msgParams],
+    //           from: address,
+    //         },
+    //         async function (err, result) {
+    //           if (err) reject(null)
+    //           if (result.error) {
+    //             reject(null)
+    //           }
+    //           resolve(result.result)
+    //         }
+    //       )
+    //     }, 1)
+    //   } catch (err) {
+    //     resolve(err)
+    //   }
+    // })
   }
 
   // return <Stack state={state} authenticate={() => send({ type: "SIGN" })} urls={stackMediaURLs} config={config} />
-  return <Upgrader apiKey={apiKey} state={state} config={config} />
+  return <Upgrader apiKey={apiKey} state={state} config={config} authenticate={() => send({ type: "SIGN" })} />
 }
 
 export default EthUpgradeWidget
