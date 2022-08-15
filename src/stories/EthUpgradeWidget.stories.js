572ac517b595213e63ec4fec5fc9ba0ae3eced5c
Gary Strader <gstrader28@yahoo.com>
2022-08-15 17:27:54 -0500

    Latest shared UI updates

--- src/stories/EthUpgradeWidget.stories.js
+++ src/stories/EthUpgradeWidget.stories.js
@@ -43,10 +43,8 @@ stories.add("Add Content", () => {
         {loaded && (
           <EthUpgradeWidget
             apiKey={apiKey}
-            contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e" //Gary wallet
-            tokenId="70460412835154751156901460371151389631452488952479197415560481284263918960641" //Gary wallet
-            // contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-            // tokenId="30553606573219150352991292921105176340809048341686170040023897679188805550081"
+            contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+            tokenId="30553606573219150352991292921105176340809048341686170040023897679188805550081"
             w3={web3}
             cb={cb}
             config={{
